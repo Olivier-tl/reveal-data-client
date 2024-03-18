5c445a9a5cfc44d7c396572742994085d532834b
Olivier Tessier-Larivière <olivier.t@bios.health>
2024-03-18 09:15:16 -0400

    Now using a str enum for StimSettingCsvColumn

--- src/reveal_data_client/stim_setting.py
+++ src/reveal_data_client/stim_setting.py
@@ -12,7 +12,7 @@ from reveal_data_client.constants import AnsPeriod
 STIM_SETTING_FILE_PATH = "primary/stim_settings.csv"
 
 
-class StimSettingCsvColumn(Enum):
+class StimSettingCsvColumn(str, Enum):
     """
     Enum to represent the columns in the CSV file containing the stimulation settings.
     """
@@ -63,11 +63,11 @@ def get_stim_settings(dataset_path: Path) -> Sequence[StimSetting]:
     stim_settings = pd.read_csv(dataset_path / STIM_SETTING_FILE_PATH)
     return [
         StimSetting(
-            stim_option=row[StimSettingCsvColumn.STIM_OPTION.value],
-            pulse_width=row[StimSettingCsvColumn.PULSE_WIDTH.value],
-            current=row[StimSettingCsvColumn.CURRENT.value],
-            frequency=row[StimSettingCsvColumn.FREQUENCY.value],
-            duty_cycle_off=row[StimSettingCsvColumn.DUTY_CYCLE_OFF.value],
+            stim_option=row[StimSettingCsvColumn.STIM_OPTION],
+            pulse_width=row[StimSettingCsvColumn.PULSE_WIDTH],
+            current=row[StimSettingCsvColumn.CURRENT],
+            frequency=row[StimSettingCsvColumn.FREQUENCY],
+            duty_cycle_off=row[StimSettingCsvColumn.DUTY_CYCLE_OFF],
         )
         for _, row in stim_settings.iterrows()
     ]
